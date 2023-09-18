0386caae4a68773093ab6ca626413a5bd96be2b8
ryv26 <ryv26@ILIY>
2023-09-18 12:52:21 +0300

    Добавьте файлы проекта.

---
+++ Form1.Designer.cs
@@ -0,0 +1,326 @@
+﻿using static System.Windows.Forms.VisualStyles.VisualStyleElement.Window;
+using System.Drawing;
+using System.Windows.Forms;
+using System;
+using System.Runtime.InteropServices;
+
+namespace DM_HW1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        /// 
+        private void InitializeComponent()
+        {
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.button1 = new System.Windows.Forms.Button();
+            this.PowerA = new System.Windows.Forms.Button();
+            this.PowerB = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.listBox3 = new System.Windows.Forms.ListBox();
+            this.Clear = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.button9 = new System.Windows.Forms.Button();
+            this.button10 = new System.Windows.Forms.Button();
+            this.button11 = new System.Windows.Forms.Button();
+            this.button12 = new System.Windows.Forms.Button();
+            this.button13 = new System.Windows.Forms.Button();
+            this.button14 = new System.Windows.Forms.Button();
+            this.button15 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(29, 63);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(37, 20);
+            this.radioButton1.TabIndex = 1;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "A";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(118, 63);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(37, 20);
+            this.radioButton2.TabIndex = 2;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "B";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(174, 22);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Добавит";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // PowerA
+            // 
+            this.PowerA.Location = new System.Drawing.Point(29, 359);
+            this.PowerA.Name = "PowerA";
+            this.PowerA.Size = new System.Drawing.Size(75, 23);
+            this.PowerA.TabIndex = 4;
+            this.PowerA.Text = "|A|";
+            this.PowerA.UseVisualStyleBackColor = true;
+            this.PowerA.Click += new System.EventHandler(this.PowerA_Click);
+            // 
+            // PowerB
+            // 
+            this.PowerB.Location = new System.Drawing.Point(110, 359);
+            this.PowerB.Name = "PowerB";
+            this.PowerB.Size = new System.Drawing.Size(75, 23);
+            this.PowerB.TabIndex = 5;
+            this.PowerB.Text = "|B|";
+            this.PowerB.UseVisualStyleBackColor = true;
+            this.PowerB.Click += new System.EventHandler(this.PowerB_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 16;
+            this.listBox1.Location = new System.Drawing.Point(30, 99);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(75, 244);
+            this.listBox1.TabIndex = 6;
+            // 
+            // listBox2
+            // 
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.ItemHeight = 16;
+            this.listBox2.Location = new System.Drawing.Point(119, 99);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.Size = new System.Drawing.Size(75, 244);
+            this.listBox2.TabIndex = 7;
+            // 
+            // listBox3
+            // 
+            this.listBox3.FormattingEnabled = true;
+            this.listBox3.ItemHeight = 16;
+            this.listBox3.Location = new System.Drawing.Point(313, 24);
+            this.listBox3.Name = "listBox3";
+            this.listBox3.Size = new System.Drawing.Size(289, 116);
+            this.listBox3.TabIndex = 8;
+            // 
+            // Clear
+            // 
+            this.Clear.Location = new System.Drawing.Point(118, 415);
+            this.Clear.Name = "Clear";
+            this.Clear.Size = new System.Drawing.Size(75, 23);
+            this.Clear.TabIndex = 9;
+            this.Clear.Text = "Clear";
+            this.Clear.UseVisualStyleBackColor = true;
+            this.Clear.Click += new System.EventHandler(this.Clear_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(338, 214);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(75, 23);
+            this.button5.TabIndex = 10;
+            this.button5.Text = "A=B";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(338, 243);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(75, 23);
+            this.button6.TabIndex = 11;
+            this.button6.Text = "A~B";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(338, 272);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(75, 23);
+            this.button7.TabIndex = 12;
+            this.button7.Text = "A<=B";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // button8
+            // 
+            this.button8.Location = new System.Drawing.Point(338, 301);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(75, 23);
+            this.button8.TabIndex = 13;
+            this.button8.Text = "B<=A";
+            this.button8.UseVisualStyleBackColor = true;
+            this.button8.Click += new System.EventHandler(this.button8_Click);
+            // 
+            // button9
+            // 
+            this.button9.Location = new System.Drawing.Point(484, 214);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(75, 23);
+            this.button9.TabIndex = 14;
+            this.button9.Text = "A и B";
+            this.button9.UseVisualStyleBackColor = true;
+            this.button9.Click += new System.EventHandler(this.button9_Click);
+            // 
+            // button10
+            // 
+            this.button10.Location = new System.Drawing.Point(484, 243);
+            this.button10.Name = "button10";
+            this.button10.Size = new System.Drawing.Size(75, 23);
+            this.button10.TabIndex = 15;
+            this.button10.Text = "A или B";
+            this.button10.UseVisualStyleBackColor = true;
+            this.button10.Click += new System.EventHandler(this.button10_Click);
+            // 
+            // button11
+            // 
+            this.button11.Location = new System.Drawing.Point(484, 272);
+            this.button11.Name = "button11";
+            this.button11.Size = new System.Drawing.Size(75, 23);
+            this.button11.TabIndex = 16;
+            this.button11.Text = "A/B";
+            this.button11.UseVisualStyleBackColor = true;
+            this.button11.Click += new System.EventHandler(this.button11_Click);
+            // 
+            // button12
+            // 
+            this.button12.Location = new System.Drawing.Point(484, 388);
+            this.button12.Name = "button12";
+            this.button12.Size = new System.Drawing.Size(75, 23);
+            this.button12.TabIndex = 17;
+            this.button12.Text = "A△B";
+            this.button12.UseVisualStyleBackColor = true;
+            this.button12.Click += new System.EventHandler(this.button12_Click);
+            // 
+            // button13
+            // 
+            this.button13.Location = new System.Drawing.Point(484, 359);
+            this.button13.Name = "button13";
+            this.button13.Size = new System.Drawing.Size(75, 23);
+            this.button13.TabIndex = 18;
+            this.button13.Text = "НЕ B";
+            this.button13.UseVisualStyleBackColor = true;
+            this.button13.Click += new System.EventHandler(this.button13_Click);
+            // 
+            // button14
+            // 
+            this.button14.Location = new System.Drawing.Point(484, 330);
+            this.button14.Name = "button14";
+            this.button14.Size = new System.Drawing.Size(75, 23);
+            this.button14.TabIndex = 19;
+            this.button14.Text = "НЕ A";
+            this.button14.UseVisualStyleBackColor = true;
+            this.button14.Click += new System.EventHandler(this.button14_Click);
+            // 
+            // button15
+            // 
+            this.button15.Location = new System.Drawing.Point(484, 301);
+            this.button15.Name = "button15";
+            this.button15.Size = new System.Drawing.Size(75, 23);
+            this.button15.TabIndex = 20;
+            this.button15.Text = "B/A";
+            this.button15.UseVisualStyleBackColor = true;
+            this.button15.Click += new System.EventHandler(this.button15_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(29, 22);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 22);
+            this.textBox1.TabIndex = 21;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button15);
+            this.Controls.Add(this.button14);
+            this.Controls.Add(this.button13);
+            this.Controls.Add(this.button12);
+            this.Controls.Add(this.button11);
+            this.Controls.Add(this.button10);
+            this.Controls.Add(this.button9);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.Clear);
+            this.Controls.Add(this.listBox3);
+            this.Controls.Add(this.listBox2);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.PowerB);
+            this.Controls.Add(this.PowerA);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.radioButton2);
+            this.Controls.Add(this.radioButton1);
+            this.Name = "Form1";
+            this.Text = "Множество Мальцев 2015";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button PowerA;
+        private System.Windows.Forms.Button PowerB;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.ListBox listBox2;
+        private System.Windows.Forms.ListBox listBox3;
+        private System.Windows.Forms.Button Clear;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button button9;
+        private System.Windows.Forms.Button button10;
+        private System.Windows.Forms.Button button11;
+        private System.Windows.Forms.Button button12;
+        private System.Windows.Forms.Button button13;
+        private System.Windows.Forms.Button button14;
+        private System.Windows.Forms.Button button15;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,243 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DM_HW1
+{
+    public partial class Form1 : Form
+    {
+        private static HashSet<string> set1 = new HashSet<string>();
+        private static HashSet<string> set2 = new HashSet<string>();
+        private static HashSet<string> universe = new HashSet<string>();
+        public Form1()
+        {
+            InitializeComponent();
+            ClearsForm();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string inputText = textBox1.Text.Trim();
+
+            if (radioButton1.Checked)
+            {
+                if (!set1.Contains(inputText))
+                {
+                    set1.Add(inputText);
+                    listBox1.Items.Add(inputText);
+                }
+                else
+                {
+                    MessageBox.Show("Этот элемент уже существует в множестве 1 ", "Повторение элемента");
+                }
+            }
+            else if (radioButton2.Checked)
+            {
+                if (!set2.Contains(inputText))
+                {
+                    set2.Add(inputText);
+                    listBox2.Items.Add(inputText);
+                }
+                else
+                {
+                    MessageBox.Show("Этот элемент уже существует в множестве 2", "Повторение элемента");
+                }
+            }
+            universe.Clear();
+            universe.UnionWith(set1);
+            universe.UnionWith(set2);
+            listBox3.Items.Clear();
+            listBox3.Items.AddRange(universe.ToArray());
+        }
+
+        private void Clear_Click(object sender, EventArgs e)
+        {
+            ClearsForm();
+        }
+        private void ClearsForm()
+        {
+            listBox1.Items.Clear();
+            listBox2.Items.Clear();
+            listBox3.Items.Clear();
+            textBox1.Text = "";
+        }
+
+        private void PowerA_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show(listBox1.Items.Count.ToString());
+        }
+
+        private void PowerB_Click(object sender, EventArgs e)
+        {
+            listBox3.Items.Clear();
+            MessageBox.Show(listBox2.Items.Count.ToString());
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            int[] set1 = listBox1.Items.Cast<string>().Select(int.Parse).ToArray();
+            int[] set2 = listBox2.Items.Cast<string>().Select(int.Parse).ToArray();
+            MessageBox.Show((set1.Length == set1.Length).ToString());
+        }
+       /* static bool AreArraysEqual<T>(T[] arr1, T[] arr2)
+        {
+            if (arr1.Length != arr2.Length)
+                return false;
+
+            var set1 = new HashSet<T>(arr1);
+            var set2 = new HashSet<T>(arr2);
+
+            return set1.SetEquals(set2);
+        }*/
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            int[] set1 = listBox1.Items.Cast<string>().Select(int.Parse).ToArray();
+            int[] set2 = listBox2.Items.Cast<string>().Select(int.Parse).ToArray();
+            MessageBox.Show((set1.Length != set1.Length).ToString());
+        }
+        private void button7_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show(IsSubset(set1, set2).ToString());
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show(IsSubset(set2, set1).ToString());
+        }
+        private static bool IsSubset<T>(IEnumerable<T> setA, IEnumerable<T> setB)
+        {
+            return setA.All(elementA => setB.Contains(elementA));
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+            string[] set1 = listBox1.Items.Cast<string>().ToArray();
+            string[] set2 = listBox2.Items.Cast<string>().ToArray();
+            MessageBox.Show(string.Join(", ", Intersection(set1, set2)));
+        }
+        static string[] Intersection(string[] set1, string[] set2)
+        {
+            HashSet<string> hashSetA = new HashSet<string>(set1);
+            HashSet<string> hashSetB = new HashSet<string>(set2);
+            hashSetA.IntersectWith(hashSetB);
+            return hashSetA.ToArray();
+        }
+
+
+        private void button10_Click(object sender, EventArgs e)
+        {
+            string[] set1 = listBox1.Items.Cast<string>().ToArray();
+            string[] set2 = listBox2.Items.Cast<string>().ToArray();
+            MessageBox.Show(string.Join(", ", Union(set1, set2)));
+        }
+        static string[] Union(string[] setA, string[] setB)
+        {
+            HashSet<string> hashSetA = new HashSet<string>(setA);
+            HashSet<string> hashSetB = new HashSet<string>(setB);
+
+            hashSetA.UnionWith(hashSetB);
+
+            return hashSetA.ToArray();
+        }
+
+        private void button11_Click(object sender, EventArgs e)
+        {
+            string result = string.Join(", ", Divide(set1, set2));
+            MessageBox.Show(result);
+        }
+        static string[] Divide(HashSet<string> setA, HashSet<string> setB)
+        {
+            setA.ExceptWith(setB);
+            return setA.ToArray();
+        }
+
+        private void button15_Click(object sender, EventArgs e)
+        {
+            string result = string.Join(", ", Divide(set2, set1));
+            MessageBox.Show(result);
+        }
+
+        private void button14_Click(object sender, EventArgs e)
+        {
+            string result = string.Join(", ", Complement(set1));
+            MessageBox.Show(result);
+        }
+
+        private void button13_Click(object sender, EventArgs e)
+        {
+            string result = string.Join(", ", Complement(set2));
+            MessageBox.Show(result);
+        }
+        public static IEnumerable<string> Complement(HashSet<string> setA)
+        {
+            var complement = universe.Except(setA);
+            return complement;
+        }
+
+
+        private void button12_Click(object sender, EventArgs e)
+        {
+            string[] result = SymmetricDifference(set1, set2);
+            string resultString = string.Join(", ", result);
+            MessageBox.Show(resultString);
+        }
+        static string[] SymmetricDifference(HashSet<string> setA, HashSet<string> setB)
+        {
+            HashSet<string> hashSetA = new HashSet<string>(setA);
+            HashSet<string> hashSetB = new HashSet<string>(setB);
+
+            hashSetA.SymmetricExceptWith(hashSetB);
+
+            return hashSetA.ToArray();
+        }
+
+        /*Тут можно откулючить Mbox*/
+        private bool isClosing = false;
+        private Form movingForm;
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (!isClosing)
+            {
+                e.Cancel = true;
+
+                movingForm = new Form();
+                movingForm.Text = "Ну что попалься мышь";
+                movingForm.FormBorderStyle = FormBorderStyle.FixedDialog;
+                movingForm.Size = new Size(400, 100);
+
+                
+                movingForm.FormClosed += MovingForm_FormClosed;
+
+                Timer timer = new Timer();
+                timer.Interval = 10; 
+                timer.Tick += Timer_Tick;
+                timer.Start();
+
+                movingForm.Show();
+
+                isClosing = true;
+            }
+        }
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+            if (movingForm != null && !movingForm.IsDisposed)
+            {
+                Point cursorPosition = Cursor.Position;
+                cursorPosition.Offset(-movingForm.Width / 2, -movingForm.Height / 2);
+                movingForm.Location = cursorPosition;
+            }
+        }
+        private void MovingForm_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            isClosing = false;
+            this.Close();
+        }
+    }
+}
